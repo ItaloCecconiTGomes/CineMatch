8ff199410673d34e2f4ba83ff29cdc3549d3665f
jrs1lva <adailtonjunior0604@gmail.com>
2026-05-06 12:13:23 -0300

    feat: class Usuario

---
+++ src/model/Usuario.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Usuario {
+	
+	private final String NOME;
+	private final int IDADE;
+	private final PerfilCinefilo PERFIL;
+	
+	public Usuario(String nome, int idade, PerfilCinefilo perfil) {
+		
+		if (nome == null || nome.isBlank()) {
+			throw new IllegalArgumentException("Nome não pode ser nulo ou vazio");
+		}
+		
+		if (idade < 0) {
+			throw new IllegalArgumentException("Idade não pode ser negativa");
+		}
+		
+		if (perfil == null) {
+			throw new IllegalArgumentException("Perfil não pode ser nulo");
+		}
+		
+		NOME = nome;
+		IDADE = idade;
+		PERFIL = perfil;
+	}
+	
+	
+	
+}
