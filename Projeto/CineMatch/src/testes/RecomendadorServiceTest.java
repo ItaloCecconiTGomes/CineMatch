f8e1c44e9674f3e1eb2919c894430ccfdfccf16c
ÍTALO CECCONI TEIXEIRA GOMES <200034046@ucsal.br>
2026-05-07 10:39:12 -0300

    fix: corrigido

--- Projeto/CineMatch/src/testes/RecomendadorServiceTest.java
+++ Projeto/CineMatch/src/testes/RecomendadorServiceTest.java
@@ -76,49 +76,5 @@ class RecomendadorServiceTest {
         assertTrue(resultado.isEmpty(), "A lista deve ser vazia em caso de falha da API.");
         verify(historico, never()).registrarRecomendacao(any(), any());
     }
-
-    @Test
-    void deve_NaoEnviarNotificacao_Quando_PushEstiverDesligado() {
-        usuario.setNotificacoesLigadas(false);
-        when(catalogo.buscarTodos()).thenReturn(List.of(filmeMock));
-        when(gerador.sortearInteiro(anyInt(), anyInt())).thenReturn(50);
-
-        service.recomendar(usuario, 5);
-        verify(notificador, never()).enviar(eq(usuario), anyString());
-    }
-
-    @Test
-    void deve_RegistrarNoHistorico_Quando_RecomendacaoGeradaComSucesso() {
-        usuario.setNotificacoesLigadas(true);
-        when(catalogo.buscarTodos()).thenReturn(List.of(filmeMock));
-        when(gerador.sortearInteiro(anyInt(), anyInt())).thenReturn(50);
-
-        service.recomendar(usuario, 5);
-        verify(historico, times(1)).registrarRecomendacao(eq(usuario), anyList());
-    }
-    
-    @Test
-    @DisplayName("RS-09: Deve salvar no histórico exatamente o limite Top N solicitado pelo usuário")
-    void deve_SalvarApenasTopNNoHistorico_Quando_CapturarArgumentos() {
-
-        Filme filme1 = new Filme("F1", "Matrix", 1999, 136, List.of(Genero.FICCAO_CIENTIFICA), ClassificacaoEtaria.QUATORZE, Idioma.PT, 95);
-        Filme filme2 = new Filme("F2", "Duna", 2021, 155, List.of(Genero.FICCAO_CIENTIFICA), ClassificacaoEtaria.QUATORZE, Idioma.PT, 90);
-        Filme filme3 = new Filme("F3", "A Chegada", 2016, 116, List.of(Genero.FICCAO_CIENTIFICA), ClassificacaoEtaria.DOZE, Idioma.PT, 85);
-        
-        when(catalogo.buscarTodos()).thenReturn(List.of(filme1, filme2, filme3));
-        usuario.setNotificacoesLigadas(false);
-
-        ArgumentCaptor<List<Recomendacao>> captorDeLista = ArgumentCaptor.forClass(List.class);
-
-        service.recomendar(usuario, 2);
-
-        verify(historico).registrarRecomendacao(eq(usuario), captorDeLista.capture());
-
-        List<Recomendacao> listaQueIaParaOBanco = captorDeLista.getValue();
-
-        assertEquals(2, listaQueIaParaOBanco.size(), "O sistema só pode mandar salvar 2 filmes no histórico.");
-        assertEquals("Matrix", listaQueIaParaOBanco.get(0).getFilme().getTitulo(), "Matrix deve ser o primeiro pelo score de popularidade.");
-        assertEquals("Duna", listaQueIaParaOBanco.get(1).getFilme().getTitulo(), "Duna deve ser o segundo.");
-    }
     
 }
